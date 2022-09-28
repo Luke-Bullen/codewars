31d94b63da5b067304a931626634def5315fbfb6
Luke Bullen <lukeabullen@gmail.com>
2022-09-28 21:33:30 +0100

    even odd challenge

---
+++ 8kyu/even_or_odd.rb
@@ -0,0 +1,12 @@
+# Instructions
+
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+# My Solution
+
+def even_or_odd(number)
+  number % 2 == 0 ? "Even": "Odd"
+end
+
+
