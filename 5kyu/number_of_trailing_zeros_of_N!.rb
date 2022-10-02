88eec9ba50ed2e87f3f6a8c55ec282bf4b4d16be
Luke Bullen <lukeabullen@gmail.com>
2022-10-03 00:08:55 +0100

    factorial challenge

--- 5kyu/number_of_trailing_zeros_of_N!.rb
+++ 5kyu/number_of_trailing_zeros_of_N!.rb
@@ -36,3 +36,15 @@ def zeros(n)
   end
   return number_of_zeros
 end
+
+
+# number of 0s depends on how many multiples of every 5 base it has eg: how many 5s + how many (5**2=125) 125s it has + how many (5**3=625) 625s it has ect.
+# create the default number of 0s to = 0 - this is given as 0 if number < 5
+# need to check how many power of 5s are needed
+# - divide by 5 and increate power counter
+# - if remaining value (number) >= 5 divide by 5 again and increase power counter and repeat unitl remaining value < 5
+# - when remaining value (number) < 5, take value of power counter as this is the indecie for which 5 is needed - eg: if power counter = 3, only 5**1 (5), 5**2 (25) and 5**3 (125) are needed
+# get range of integers from 1 upto and including the power counter
+# iterate through each vaue of the range and divide the initial input number by 5**range number and add the result to the number_of_zeros - round down to closest integer
+# repeat for every value in the range
+# return the number_of_zeors
