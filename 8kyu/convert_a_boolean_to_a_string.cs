e798cdee0324def272f40a1d26a4fa42840f74ad
Luke Bullen <lukeabullen@gmail.com>
2022-11-10 23:24:09 +0000

    c# fundermentals

---
+++ 8kyu/convert_a_boolean_to_a_string.cs
@@ -0,0 +1,15 @@
+// Instructions:
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// My Solution:
+
+using System;
+
+public class kata
+{
+  public static string boolean_to_string(bool b)
+  {
+    return (b.ToString());
+  }
+}
